8b9db068ea56f3deccc139c9625d6b21d44038ae
Ashlyn708 <averett_ashlyn@columbusstate.edu>
2020-10-17 14:35:57 +0000

    working comics

--- index.js
+++ index.js
@@ -17,36 +17,34 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-//Couple of items todo
-var tasks = [];
-//completed items
-var completed = [];
 
 //get home page /
 app.get('/', function(req, res){
     let comicData;
     fetch('http://xkcd.com/info.0.json')
+    .then(res => res.json())
+    .then(data => {
+        res.render('index',{data:data})
     console.log("got it");
-    res.render('index');
+    })
+    
 });
 
-app.get('/nasa', function(req, res){
-    let nasaData;
-    fetch('https://api.nasa.gov/planetary/apod?api_key=7lqr4qoVCaJweZv9hp89XHb6he3UEqesrowGwAMa')
+app.get('/random', function(req, res){
+    let randNum=Math.floor((Math.random() * 2373) + 1);
+    fetch('https://xkcd.com/'+randNum+'/info.0.json')
     .then(res => res.json())
     .then(data => {
-        res.render('nasa',{data:data})
+        res.render('random',{data:data})
     });
 })
-//fetch nasa information and send to front end as JSON data\
-//change nasa to comic
-app.get('/nasaDate', function(req, res){
-    let nasaData;
-    fetch('https://api.nasa.gov/planetary/apod?api_key=7lqr4qoVCaJweZv9hp89XHb6he3UEqesrowGwAMa')
+
+app.post('/newRand', function(req, res){
+    let randNum=Math.floor((Math.random() * 2373) + 1);
+    fetch('https://xkcd.com/'+randNum+'/info.0.json')
     .then(res => res.json())
     .then(data => {
-        nasaData = data;
-        res.json(nasaData);
+        res.render('random',{data:data})
     });
 })
 
